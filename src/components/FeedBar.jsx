67940e64b5b68a66334e33f8afcd7f1c9d698239
szu_bee <596460499@qq.com>
2017-10-18 14:05:41 +0800

    change layout and add font awesome

--- src/components/FeedBar.jsx
+++ src/components/FeedBar.jsx
@@ -1,29 +1,66 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import '../styles/FeedBar.css';
+import '../assets/font-awesome-4.7.0/css/font-awesome.min.css';
 
 import rssLogo from '../assets/rss.png';
-import catPhoto from '../assets/cat-pet-animal-domestic-104827.jpeg';
 import slothPhoto from '../assets/sloth.jpg';
+import rssIcon from '../assets/RSS-icon.png';
 
 function FeedList(props) {
   const feedMsgs = props.feedMsgs;
 
-  const listItems = feedMsgs.map(feedMsg =>
-    <li className="feed-list">
-      <img src={slothPhoto} alt=""/>
+  const listItems = feedMsgs.map(feedMsg => <li className="feed-list">
+    <div>
+      <img src={rssIcon} alt=""/>
       <span>{feedMsg.feedTitle}</span>
-      <span className="article-count">{feedMsg.articleCount}</span>
-    </li>
-  );
+    </div>
+    <span className="article-count">{feedMsg.articleCount}</span>
+  </li>);
   return (
-    <ul className="feed-lists">{listItems}</ul>
+    <div className="feed-lists-content">
+      <ul className="feed-lists">{listItems}</ul>
+    </div>
   );
 }
 
 const feedMsgs = [
-  {feedTitle: 'hhh', articleCount: 11},
-  {feedTitle: 'dkgs', articleCount: 22},
-  {feedTitle: 'skf', articleCount: 33}
+  {
+    feedTitle: 'hhh',
+    articleCount: 11
+  }, {
+    feedTitle: 'dkgs',
+    articleCount: 22
+  }, {
+    feedTitle: 'skf',
+    articleCount: 33
+  }, {
+    feedTitle: 'dskjf',
+    articleCount: 66
+  }, {
+    feedTitle: 'sfs',
+    articleCount: 23
+  }, {
+    feedTitle: 'sfsdfs',
+    articleCount: 33
+  }, {
+    feedTitle: 'allk',
+    articleCount: 34
+  }, {
+    feedTitle: 'skf',
+    articleCount: 33
+  }, {
+    feedTitle: 'dskjf',
+    articleCount: 66
+  }, {
+    feedTitle: 'sfs',
+    articleCount: 23
+  }, {
+    feedTitle: 'sfsdfs',
+    articleCount: 33
+  }, {
+    feedTitle: 'allk',
+    articleCount: 34
+  }
 ]
 
 class FeedBar extends Component {
@@ -31,20 +68,23 @@ class FeedBar extends Component {
     return (
       <div className="feed-bar">
         <div className="logo-block">
-          <img src={rssLogo} alt="rss-logo" />
+          <img src={rssLogo} alt="rss-logo"/>
           <div>RSS</div>
           <div className="date">Today, 1:43PM</div>
         </div>
         <div className="lists">
           <div className="unread">
-            <img src={catPhoto} alt="unread" />
-            <span>Unread</span>
+            <div>
+              <img src={slothPhoto} alt="unread"/>
+              <span>Unread</span>
+            </div>
             <span className="article-count">8888</span>
           </div>
-          <FeedList feedMsgs={feedMsgs} />
+          <FeedList feedMsgs={feedMsgs}/>
         </div>
         <footer>
-          <input className="subscribe" />
+          <i className="fa fa-refresh"></i>
+          <i className="fa fa-plus"></i>
         </footer>
       </div>
     );
