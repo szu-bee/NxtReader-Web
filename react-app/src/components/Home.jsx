598be8562af54cbb4ecc8750cb3681f32ea85271
szu_bee <596460499@qq.com>
2017-12-11 20:42:33 +0800

    add LoginForm and RegistrationForm

--- react-app/src/components/Home.jsx
+++ react-app/src/components/Home.jsx
@@ -1,55 +1,139 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import '../styles/css/Home.css'
+import { Modal, Button } from 'antd'
 
-function TopBar() {
-  return (
-    <div id="top-bar">
-      <div className="logo">
-        <a href="/">NxtReader</a>
-      </div>
-      <div className="options">
-        <div className="app-types">
-          <a href="#"
-            className="underlink_hover">
-            iOS App
-          </a>
-          <a href="#"
-            className="underlink_hover">
-            Android App
-          </a>
-          <a href="#"
-            className="underlink_hover">
-            Desktop (Electron) App
-          </a>
+import LoginForm from './Login'
+import RegistrationForm from './Registration'
+
+class TopBar extends Component {
+  state = {
+    visible: false,
+    confirmLoading: false
+  }
+
+  showModal = () => {
+    this.setState({
+      visible: true
+    })
+  }
+
+  handleOk = () => {
+    this.setState({
+      confirmLoading: true
+    })
+
+    setTimeout(() => {
+      this.setState({
+        visible: false,
+        confirmLoading: false
+      })
+    }, 1000)
+  }
+
+  handleCancel = () => {
+    this.setState({
+      visible: false
+    })
+  }
+
+  render() {
+    const { visible, confirmLoading } = this.state
+    
+    return (
+      <div id="top-bar">
+        <div className="logo">
+          <a href="/">NxtReader</a>
         </div>
-        <div className="sign-in">
-          <a href="javascript:void(0);" 
-            className="login" 
-            className="underlink_hover">
-            Sign in
-          </a>
+        <div className="options">
+          <div className="app-types">
+            <a href="#"
+              className="underlink_hover">
+              iOS App
+            </a>
+            <a href="#"
+              className="underlink_hover">
+              Android App
+            </a>
+            <a href="#"
+              className="underlink_hover">
+              Desktop (Electron) App
+            </a>
+          </div>
+          <div className="sign-in">
+            <Button type="primary" onClick={ this.showModal }>
+              Sign in  
+            </Button>
+            <Modal title="Sign in"
+              visible={ visible }
+              onOk={ this.handleOk }
+              confirmLoading={ confirmLoading }
+              onCancel={ this.handleCancel }>
+              <LoginForm />
+            </Modal>
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
-function MiddleBlock() {
-  return (
-    <div id="middle-block">
-      <h3>
-        The content reader for power users who want to save time.
-      </h3>
-      <div className="deep-bg">
-        <div>
-          Discover how you can get the most value out of your reading!
+class MiddleBlock extends Component {
+  state = {
+    visible: false,
+    confirmLoading: false
+  }
+
+  showModal = () => {
+    this.setState({
+      visible: true
+    })
+  }
+
+  handleOk = () => {
+    this.setState({
+      confirmLoading: true
+    })
+
+    setTimeout(() => {
+      this.setState({
+        visible: false,
+        confirmLoading: false
+      })
+    }, 1000)
+  }
+
+  handleCancel = () => {
+    this.setState({
+      visible: false
+    })
+  }
+
+  render() {
+    const { visible, confirmLoading } = this.state
+
+    return (
+      <div id="middle-block">
+        <h3>
+          The content reader for power users who want to save time.
+        </h3>
+        <div className="deep-bg">
+          <div>
+            Discover how you can get the most value out of your reading!
+          </div>
+          <button onClick={ this.showModal }>
+            Create a free account
+          </button>
+          <Modal title="Register"
+            visible={ visible }
+            onOk={ this.handleOk }
+            confirmLoading={ confirmLoading }
+            onCancel={ this.handleCancel }>
+            <RegistrationForm />
+          </Modal>
         </div>
-        <button>
-          Create a free account
-        </button>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 function Footer() {
---
+++ react-app/src/components/Registration.jsx
@@ -0,0 +1,183 @@
+import React, { Component } from 'react'
+import '../styles/css/Registration.css'
+import { Form, Input, Tooltip, Icon, Cascader, Select, Row, Col, Checkbox, Button, AutoComplete } from 'antd'
+
+const FormItem = Form.Item
+const Option = Select.Option
+const AutoCompleteOption = AutoComplete.Option
+
+class RegistrationForm extends React.Component {
+  state = {
+    confirmDirty: false,
+    autoCompleteResult: [],
+  }
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.form.validateFieldsAndScroll((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values)
+      }
+    })
+  }
+  handleConfirmBlur = (e) => {
+    const value = e.target.value
+    this.setState({ confirmDirty: this.state.confirmDirty || !!value })
+  }
+  checkPassword = (rule, value, callback) => {
+    const form = this.props.form
+    if (value && value !== form.getFieldValue('password')) {
+      callback('Two passwords that you enter is inconsistent!')
+    } else {
+      callback()
+    }
+  }
+  checkConfirm = (rule, value, callback) => {
+    const form = this.props.form
+    if (value && this.state.confirmDirty) {
+      form.validateFields(['confirm'], { force: true })
+    }
+    callback()
+  }
+
+  render() {
+    const { getFieldDecorator } = this.props.form
+    const { autoCompleteResult } = this.state
+
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 8 },
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 16 },
+      },
+    }
+    const tailFormItemLayout = {
+      wrapperCol: {
+        xs: {
+          span: 24,
+          offset: 0,
+        },
+        sm: {
+          span: 16,
+          offset: 8,
+        },
+      },
+    }
+    const prefixSelector = getFieldDecorator('prefix', {
+      initialValue: '86',
+    })(
+      <Select style={{ width: 70 }}>
+        <Option value="86">+86</Option>
+        <Option value="87">+87</Option>
+      </Select>
+    )
+
+    return (
+      <Form onSubmit={this.handleSubmit} className="registration-form">
+        <FormItem
+          {...formItemLayout}
+          label="E-mail"
+        >
+          {getFieldDecorator('email', {
+            rules: [{
+              type: 'email', message: 'The input is not valid E-mail!',
+            }, {
+              required: true, message: 'Please input your E-mail!',
+            }],
+          })(
+            <Input />
+          )}
+        </FormItem>
+        <FormItem
+          {...formItemLayout}
+          label="Password"
+        >
+          {getFieldDecorator('password', {
+            rules: [{
+              required: true, message: 'Please input your password!',
+            }, {
+              validator: this.checkConfirm,
+            }],
+          })(
+            <Input type="password" />
+          )}
+        </FormItem>
+        <FormItem
+          {...formItemLayout}
+          label="Confirm Password"
+        >
+          {getFieldDecorator('confirm', {
+            rules: [{
+              required: true, message: 'Please confirm your password!',
+            }, {
+              validator: this.checkPassword,
+            }],
+          })(
+            <Input type="password" onBlur={this.handleConfirmBlur} />
+          )}
+        </FormItem>
+        <FormItem
+          {...formItemLayout}
+          label={(
+            <span>
+              Nickname
+              <Tooltip title="What do you want others to call you?">
+                <Icon type="question-circle-o" />
+              </Tooltip>
+            </span>
+          )}
+        >
+          {getFieldDecorator('nickname', {
+            rules: [{ required: true, message: 'Please input your nickname!', whitespace: true }],
+          })(
+            <Input />
+          )}
+        </FormItem>
+        <FormItem
+          {...formItemLayout}
+          label="Phone Number"
+        >
+          {getFieldDecorator('phone', {
+            rules: [{ required: true, message: 'Please input your phone number!' }],
+          })(
+            <Input addonBefore={prefixSelector} style={{ width: '100%' }} />
+          )}
+        </FormItem>
+        <FormItem
+          {...formItemLayout}
+          label="Captcha"
+          extra="We must make sure that your are a human."
+        >
+          <Row gutter={8}>
+            <Col span={12}>
+              {getFieldDecorator('captcha', {
+                rules: [{ required: true, message: 'Please input the captcha you got!' }],
+              })(
+                <Input />
+              )}
+            </Col>
+            <Col span={12}>
+              <Button>Get captcha</Button>
+            </Col>
+          </Row>
+        </FormItem>
+        <FormItem {...tailFormItemLayout}>
+          {getFieldDecorator('agreement', {
+            valuePropName: 'checked',
+          })(
+            <Checkbox>I have read the <a href="">agreement</a></Checkbox>
+          )}
+        </FormItem>
+        <FormItem {...tailFormItemLayout}>
+          <Button type="primary" htmlType="submit" className="registration-form-button">Register</Button>
+        </FormItem>
+      </Form>
+    )
+  }
+}
+
+const WrappedRegistrationForm = Form.create()(RegistrationForm)
+
+export default WrappedRegistrationForm
